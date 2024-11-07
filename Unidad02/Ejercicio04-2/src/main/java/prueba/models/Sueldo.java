def3ef5f698b936de7c058c5175c2dfe10778e92
Santiago Manuel Tamayo Arozamena <145398719+DAMSanti@users.noreply.github.com>
2024-11-07 20:10:11 +0100

    .

---
+++ Unidad02/Ejercicio04-2/src/main/java/prueba/models/Sueldo.java
@@ -0,0 +1,30 @@
+package prueba.models;
+
+public class Sueldo {
+    private double salario;
+    private double comision;
+
+    public Sueldo() {
+    }
+
+    public Sueldo(double salario, double comision) {
+        this.salario = salario;
+        this.comision = comision;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public double getComision() {
+        return comision;
+    }
+
+    public void setComision(double comision) {
+        this.comision = comision;
+    }
+}
